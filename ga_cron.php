97fc69a75724fcd5f81f049908e9efea79f19d6a
Paul Zellmer <paulzellmer9@gmail.com>
2014-12-19 08:44:43 -0800

    cron job fix

--- ga_cron.php
+++ ga_cron.php
@@ -1,5 +1,5 @@
 <?
-require 'JShrink.php';
+require 'JShrink/Minifier.php';
 
 function getRemoteFile($remoteFile, $localfile){
   // Connection time out
@@ -69,14 +69,14 @@ function getRemoteFile($remoteFile, $localfile){
 $remoteFile = 'http://google-analytics.com/analytics.js';
 $localfile = '/home/pazell/public_html/analytics.js';
 getRemoteFile($remoteFile, $localfile);
-$minLocalfile = JShrink::minify($localfile, array('flaggedComments' => false));
+$minLocalfile = \JShrink\Minifier::minify(file_get_contents($localfile), array('flaggedComments' => false));
 file_put_contents('analytics.min.js', $minLocalfile);
 
 //For Cpanel it will be /home/USERNAME/public_html/ga.js
 $remoteFile2 = 'http://www.google-analytics.com/plugins/ua/linkid.js';
 $localfile2 = '/home/pazell/public_html/linkid.js';
 getRemoteFile($remoteFile2, $localfile2);
-$minLocalfile2 = JShrink::minify($localfile2, array('flaggedComments' => false));
+$minLocalfile2 = \JShrink\Minifier::minify(file_get_contents($localfile2), array('flaggedComments' => false));
 file_put_contents('linkid.min.js', $minLocalfile2);
 
 ?>
